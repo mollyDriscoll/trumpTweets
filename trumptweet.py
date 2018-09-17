d42033adc651f70df6118ac71f5a0a6876a0a678
mollyDriscoll <mollydriscoll@Mollys-MBP-4.home>
2018-09-17 17:29:50 -0400

    First commit

---
+++ trumptweet.py
@@ -0,0 +1,45 @@
+import urllib
+from bs4 import BeautifulSoup
+import csv
+
+#to run: pipenv run python trumptweet.py
+from twitter_scraper import get_tweets
+tweets = [[]]
+for tweet in get_tweets('realDonaldTrump', pages=1):
+    #print(tweet['text'], tweet['time'])
+    row = [tweet['text'], tweet[time]]
+    tweets.append(row)
+
+
+
+
+with open('person.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(tweets)
+
+csvFile.close()
+
+
+
+
+
+
+#get the archived tweets
+# archivePage = 'http://www.trumptwitterarchive.com/archive/loser'
+# page = urllib.request.urlopen(archivePage)
+# soup = BeautifulSoup(page, 'html.parser')
+
+# print(soup)
+
+
+
+#we have the tweets, what now?
+#set up website?
+#analyze
+	#top words in a given time period?
+	#top topics
+	#nicknames
+	#emotion
+	#capitilization
+	#tweet popularity
+		#popular in which groups
